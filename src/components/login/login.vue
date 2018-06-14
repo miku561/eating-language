f11233aaed6b085b00cc946eaf60a099f6b8c9de
miku561 <miku561@163.com>
2018-06-14 18:02:25 +0800

    增加scss语法环境

---
+++ src/components/login/login.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="login">
+    <div class="logo-wrap">
+      <img src="./logo.png" alt="" class="logo-img">
+    </div>
+    <div class="form-part">
+      <div class="form-group">
+        <i class="icon icon-user"></i>
+        <input type="text" placeholder="用户名">
+      </div>
+      <div class="form-group">
+        <i class="icon icon-pwd"></i>
+        <input type="password" placeholder="password">
+      </div>
+      <div class="form-group">
+        <button class="submit-btn">登 录</button>
+      </div>
+      <div class="form-group">
+        <span class="forget-pwd fl">注册</span> 
+        <span class="forget-pwd fr">忘记密码</span> 
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{}
+</script>
+
+<style scoped lang="scss">
+  
+</style>
