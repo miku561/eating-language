fc9a5cd888b12249b7e909f24abe66ae786cfc2c
miku561 <miku561@163.com>
2018-06-15 14:34:14 +0800

    添加axios vuex插件

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -13,20 +13,35 @@
         <input type="password" placeholder="password">
       </div>
       <div class="form-group">
-        <button class="submit-btn">登 录</button>
+        <button class="submit-btn" @click="login">登 录</button>
       </div>
       <div class="form-group">
-        <span class="forget-pwd fl">注册</span> 
-        <span class="forget-pwd fr">忘记密码</span> 
+        <span class="forget-pwd fl">注册</span>
+        <span class="forget-pwd fr">忘记密码</span>
       </div>
     </div>
   </div>
 </template>
 
-<script>
-  export default{}
+<script >
+export default{
+  methods: {
+    login () {
+      this.$axios({
+        method: 'POST',
+        url: 'http://47.96.101.254/bootdo-wms/login',
+        data: {
+          'username': 'admin',
+          'password': '111111'
+        }
+      }).then((res) => {
+        console.log(res)
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang="scss">
-  
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  axios,
   components: { App },
   template: '<App/>'
 })
